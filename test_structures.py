37136b3d2d9545c90ec97172a310ca07b5b26143
myasutake <mark.yasutake@gmail.com>
2018-05-03 13:06:10 -0700

    Add tests for Stack data structure class

--- test_structures.py
+++ test_structures.py
@@ -63,3 +63,73 @@ class TestLinkedList:
             pass
         else:
             assert False, 'Expected an Exception, but one was not raised.'
+
+class TestStack:
+
+    @pytest.fixture(scope='function')
+    def test_stack(self):
+        x = data_structures.Stack()
+        x.push('lorem')
+        x.push('dim')
+        x.push('sum')
+        x.push('Cha')
+        x.push('siu')
+        x.push('bao')
+        return x
+
+    def test_peek_return_type(self, test_stack):
+        peeked = test_stack.peek()
+        assert type(peeked) == data_structures.Node
+
+    def test_peek_value(self, test_stack):
+        peeked = test_stack.peek()
+        assert peeked.data == 'bao'
+        peeked = test_stack.peek()
+        assert peeked.data == 'bao'
+        peeked = test_stack.peek()
+        assert peeked.data == 'bao'
+
+    def test_peek_size(self, test_stack):
+        test_stack.peek()
+        assert test_stack.size == 6
+        test_stack.peek()
+        assert test_stack.size == 6
+        test_stack.peek()
+        assert test_stack.size == 6
+
+    def test_pop_return_type(self, test_stack):
+        popped = test_stack.pop()
+        assert type(popped) == data_structures.Node
+
+    def test_pop_value(self, test_stack):
+        popped = test_stack.pop()
+        assert popped.data == 'bao'
+        popped = test_stack.pop()
+        assert popped.data == 'siu'
+        popped = test_stack.pop()
+        assert popped.data == 'Cha'
+
+    def test_pop_size(self, test_stack):
+        test_stack.pop()
+        assert test_stack.size == 5
+        test_stack.pop()
+        assert test_stack.size == 4
+        test_stack.pop()
+        assert test_stack.size == 3
+
+    def test_pop_empty(self):
+        test_stack = data_structures.Stack()
+        try:
+            popped = test_stack.pop()
+        except:
+            pass
+        else:
+            assert False, "Expected an exception; instead got a Node with value '{}'.".format(popped.data)
+
+    def test_push_return_type(self, test_stack):
+        pushed = test_stack.push('steamed')
+        assert pushed == None
+
+    def test_push_size(self, test_stack):
+        test_stack.push('steamed')
+        assert test_stack.size == 7
